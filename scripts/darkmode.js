58b51345637ed5ca5c4227f2b8130e299280c59e
Kayc <kaycmendes@gmail.com>
2020-08-18 03:38:55 -0300

    add dark mode

---
+++ scripts/darkmode.js
@@ -0,0 +1,26 @@
+function toggleDarkMode(newState) {
+    if (newState == "on") {
+        DarkReader.enable();
+        document.querySelector('.on').style.display = "block"
+        document.querySelector('.button').style.backgroundColor="rgb(125, 64, 138)";
+        document.querySelector('.gh').style.filter="invert(1)"
+    }
+    
+    if (newState === 'off') {
+        DarkReader.disable();
+        document.querySelector('.gh').style.filter="invert(0)"
+        document.querySelector('.button').style.backgroundColor="white";
+        document.querySelector('.on').style.display = 'none'
+
+    }
+}
+
+
+document.querySelector(".off").addEventListener("click", function () {
+    toggleDarkMode("on");
+});
+document.querySelector(".on").addEventListener("click", function () {
+    toggleDarkMode("off");
+});
+
+toggleDarkMode(newState = "off")
